fb5648da33dfaad6bde982cb85995c6f519670bb
EDWINvincible <edwinvincent.evt@gmail.com>
2022-01-06 23:12:48 +0530

    bst

--- BSTree.java
+++ BSTree.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class BSTree {
-    public class Node{
+    static Scanner sc = null;
+    static class Node{
         Node left, right;
         int data;
 
@@ -10,14 +11,72 @@ public class BSTree {
         }
     }
 
-    public Node createTree(){
+    static Node createTree(){
         Node root = null;
-        Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter Data :");
+        int data = sc.nextInt();
+        if(data == -1) 
+            return null;
+        root = new Node(data);  
+        System.out.println("Enter Left of "+data);  
+        root.left = createTree();
+        System.out.println("Enter Rigth of "+data);
+        root.right = createTree();
         return root;
     }
 
+    static void inOrder(Node root){
+        if(root == null) 
+            return;
+        inOrder(root.left);
+        System.out.print(" "+root.data+" ");
+        inOrder(root.right);
+    }
+
+    static void preOrder(Node root){
+        if(root == null) 
+            return;
+        System.out.print(" "+root.data+" ");    
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    static void postOrder(Node root){
+        if(root == null) 
+            return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(" "+root.data+" ");   
+    }
+
+    static Node searchKey(Node root, int key){
+        if(root==null || root.data == key)
+            return root;
+        if(key<root.data)
+            return searchKey(root.left, key);
+        else    
+            return searchKey(root.right, key);                     
+    }
+
+    static int heightTree(Node root){
+        if(root == null)
+            return 0;
+        return Math.max(heightTree(root.left),heightTree(root.right))+1;
+    }
     public static void main(String[] args){
-        
+        sc = new Scanner(System.in);
+        Node root = createTree();
+        System.out.print("\nInorder :");
+        inOrder(root);
+        System.out.print("\nPreorder :");
+        preOrder(root);
+        System.out.print("\nPostorder :");
+        postOrder(root);
+        // System.out.print("Enter key");
+        // int key = sc.nextInt();
+        // Node root1 = searchKey(root, key);
+        // System.out.println("Root found :"+root1.data);
+        int height = heightTree(root);
+        System.out.println("Height of tree is :"+height);
     }
 }
